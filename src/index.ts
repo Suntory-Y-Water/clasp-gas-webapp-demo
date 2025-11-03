b9e2beba5f2da723a70d601336c84fa36805a7af
sui <1202ayasnppk@gmail.com>
2025-11-03 09:23:07 +0900

    feat: HTMLを作成

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,24 @@ function main(): void {
 
   console.log({ hiMessage });
 }
+
+/**
+ * WebアプリケーションのエントリーポイントとなるGET リクエストハンドラ
+ *
+ * Google Apps Script の Web アプリケーション機能により、
+ * このURLにアクセスした際に app/index.html を返す。
+ *
+ * @returns {GoogleAppsScript.HTML.HtmlOutput} HTMLページ
+ *
+ * @example
+ * // ブラウザから以下のようなURLでアクセスすると実行される
+ * // https://script.google.com/macros/s/{SCRIPT_ID}/exec
+ */
+// @ts-ignore
+// biome-ignore lint/correctness/noUnusedVariables: GASのエントリーポイントとして必要
+function doGet(): GoogleAppsScript.HTML.HtmlOutput {
+  // 実際のHTMLがあるファイル名を指定
+  return HtmlService.createHtmlOutputFromFile('app/index')
+    .setTitle('Drive検索ビューア')
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+}
